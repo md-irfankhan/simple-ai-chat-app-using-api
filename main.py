6f037f252961f04412bc4ef021ae166991a296f8
Md. Irfan Khan <learn.irfankhan@gmail.com>
2026-09-12 15:27:16 +0600

    post method added to chat

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Body
+from services.chats import get_chat
 app=FastAPI()
 @app.get('/')
 def home():
     return "Hello"
+@app.post('/chat')
+def chat(message:str=Body(None)):
+    if len(message)<0:
+        return "You must input some text"
+    return f"""
+    you: {message}
+    assistant:{get_chat(message=message)}
+    """
